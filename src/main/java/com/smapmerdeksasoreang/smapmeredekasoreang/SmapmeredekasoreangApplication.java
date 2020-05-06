6f89e48dc14c750155fe73fa57df303da34d2688
nfadhil13 <48423700+nfadhil13@users.noreply.github.com>
2020-05-07 01:25:34 +0700

    add servlet

--- src/main/java/com/smapmerdeksasoreang/smapmeredekasoreang/SmapmeredekasoreangApplication.java
+++ src/main/java/com/smapmerdeksasoreang/smapmeredekasoreang/SmapmeredekasoreangApplication.java
@@ -2,9 +2,10 @@ package com.smapmerdeksasoreang.smapmeredekasoreang;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SmapmeredekasoreangApplication {
+public class SmapmeredekasoreangApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SmapmeredekasoreangApplication.class, args);
--- src/main/java/com/smapmerdeksasoreang/smapmeredekasoreang/configuration/WebConfiguration.java
+++ src/main/java/com/smapmerdeksasoreang/smapmeredekasoreang/configuration/WebConfiguration.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
 
 @Configuration
 public class WebConfiguration implements WebMvcConfigurer {
