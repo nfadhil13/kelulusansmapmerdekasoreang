a2ebaaf9b83dc759d9a79e9fbf03c941f4f23519
nfadhil13 <48423700+nfadhil13@users.noreply.github.com>
2020-05-07 00:37:41 +0700

    try

--- src/main/java/com/smapmerdeksasoreang/smapmeredekasoreang/dao/SiswaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.smapmerdeksasoreang.smapmeredekasoreang.dao;
-
-import com.smapmerdeksasoreang.smapmeredekasoreang.model.Siswa;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SiswaRepository extends MongoRepository<Siswa, String>{
-}
