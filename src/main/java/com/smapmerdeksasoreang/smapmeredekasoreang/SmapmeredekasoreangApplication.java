a7792679a0eb0f3826fe0376d9388ef0f0f8d6dc
nfadhil13 <48423700+nfadhil13@users.noreply.github.com>
2020-05-07 00:15:03 +0700

    FirstTry

---
+++ src/main/java/com/smapmerdeksasoreang/smapmeredekasoreang/SmapmeredekasoreangApplication.java
@@ -0,0 +1,13 @@
+package com.smapmerdeksasoreang.smapmeredekasoreang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmapmeredekasoreangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmapmeredekasoreangApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smapmerdeksasoreang/smapmeredekasoreang/SmapmeredekasoreangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smapmerdeksasoreang.smapmeredekasoreang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmapmeredekasoreangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
